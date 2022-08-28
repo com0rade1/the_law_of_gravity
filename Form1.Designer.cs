c27cfc90d44cd05b5b0882e796bb90b91773f73f
com0rade1 <protasovnekit@gmail.com>
2022-08-28 20:32:22 +0300

    app uploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Закон_всемирного_тяготения
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxX0 = new System.Windows.Forms.TextBox();
+            this.textBoxVX0 = new System.Windows.Forms.TextBox();
+            this.textBoxVY0 = new System.Windows.Forms.TextBox();
+            this.textBoxStep = new System.Windows.Forms.TextBox();
+            this.textBoxM = new System.Windows.Forms.TextBox();
+            this.textBoxG = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTimer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(422, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Абсцисса начального положения тела";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Абсцисса начальной скорости";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ордината начальной скорости";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(422, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Масса планеты";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(422, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Шаг вычислений(времени)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(422, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Гравитационная постоянная";
+            // 
+            // textBoxX0
+            // 
+            this.textBoxX0.Location = new System.Drawing.Point(630, 12);
+            this.textBoxX0.Name = "textBoxX0";
+            this.textBoxX0.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX0.TabIndex = 6;
+            this.textBoxX0.Text = "10";
+            // 
+            // textBoxVX0
+            // 
+            this.textBoxVX0.Location = new System.Drawing.Point(630, 35);
+            this.textBoxVX0.Name = "textBoxVX0";
+            this.textBoxVX0.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVX0.TabIndex = 7;
+            this.textBoxVX0.Text = "0,1";
+            // 
+            // textBoxVY0
+            // 
+            this.textBoxVY0.Location = new System.Drawing.Point(630, 59);
+            this.textBoxVY0.Name = "textBoxVY0";
+            this.textBoxVY0.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVY0.TabIndex = 8;
+            this.textBoxVY0.Text = "0,1";
+            // 
+            // textBoxStep
+            // 
+            this.textBoxStep.Location = new System.Drawing.Point(630, 85);
+            this.textBoxStep.Name = "textBoxStep";
+            this.textBoxStep.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStep.TabIndex = 9;
+            this.textBoxStep.Text = "1";
+            // 
+            // textBoxM
+            // 
+            this.textBoxM.Location = new System.Drawing.Point(630, 109);
+            this.textBoxM.Name = "textBoxM";
+            this.textBoxM.Size = new System.Drawing.Size(100, 20);
+            this.textBoxM.TabIndex = 10;
+            this.textBoxM.Text = "5,97";
+            // 
+            // textBoxG
+            // 
+            this.textBoxG.Location = new System.Drawing.Point(630, 135);
+            this.textBoxG.Name = "textBoxG";
+            this.textBoxG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxG.TabIndex = 11;
+            this.textBoxG.Text = "6,67408";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(425, 194);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(305, 23);
+            this.StartButton.TabIndex = 13;
+            this.StartButton.Text = "Задать данные";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(425, 223);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(305, 23);
+            this.PauseButton.TabIndex = 14;
+            this.PauseButton.Text = "Запуск";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(422, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Скорость отрисовки";
+            // 
+            // textBoxTimer
+            // 
+            this.textBoxTimer.Location = new System.Drawing.Point(630, 164);
+            this.textBoxTimer.Name = "textBoxTimer";
+            this.textBoxTimer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTimer.TabIndex = 18;
+            this.textBoxTimer.Text = "32";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Рассчитать первую космическую скорость";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(305, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Рассчитать вторую космическую скорость";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTimer);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxG);
+            this.Controls.Add(this.textBoxM);
+            this.Controls.Add(this.textBoxStep);
+            this.Controls.Add(this.textBoxVY0);
+            this.Controls.Add(this.textBoxVX0);
+            this.Controls.Add(this.textBoxX0);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxX0;
+        private System.Windows.Forms.TextBox textBoxVX0;
+        private System.Windows.Forms.TextBox textBoxVY0;
+        private System.Windows.Forms.TextBox textBoxStep;
+        private System.Windows.Forms.TextBox textBoxM;
+        private System.Windows.Forms.TextBox textBoxG;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Закон_всемирного_тяготения
+{
+    public partial class Form1 : Form
+    {
+        double x0, y0, x1, y1, h, M, vx0, vy0, k,tempx,tempy;
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(textBoxTimer.Text);
+            timer1.Start();
+            timer1.Enabled = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBoxVY0.Text = Convert.ToString(Math.Sqrt(2)*Math.Sqrt(k * M / Convert.ToDouble(textBoxX0.Text)));
+            textBoxVX0.Text = "0";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBoxVX0.Text = "0";
+            textBoxVY0.Text = Convert.ToString(Math.Sqrt(k * M / Convert.ToDouble(textBoxX0.Text)));
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            tempx = x1 * (((-k * M) / Math.Pow((Math.Sqrt(x1 * x1 + y1 * y1)), 3)) * h * h + 2) - x0;
+            tempy = y1 * (((-k * M) / Math.Pow((Math.Sqrt(x1 * x1 + y1 * y1)), 3)) * h * h + 2) - y0;
+            g.DrawLine(new Pen(Color.Black), pictureBox1.Width / 2 + (float)x1, pictureBox1.Height / 2 + (float)y1, pictureBox1.Width / 2 + (float)tempx, pictureBox1.Height / 2 + (float)tempy);
+            x0 = x1;
+            y0 = y1;
+            x1 = tempx;
+            y1 = tempy;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            x0 = Convert.ToDouble(textBoxX0.Text);
+            y0 = 0;
+            h = Convert.ToDouble(textBoxStep.Text);
+            vx0 = Convert.ToDouble(textBoxVX0.Text);
+            vy0 = Convert.ToDouble(textBoxVY0.Text);
+            M = Convert.ToDouble(textBoxM.Text);
+            k = Convert.ToDouble(textBoxG.Text);
+            x1 = x0 + h * vx0;
+            y1 = y0 + h * vy0;
+            g.DrawEllipse(new Pen(Color.Black, 2), pictureBox1.Width / 2 - 20, pictureBox1.Height / 2 - 20, 40,40);
+            g.DrawLine(new Pen(Color.Black), pictureBox1.Width / 2 + (float)x0, pictureBox1.Height / 2 + (float)y0, pictureBox1.Width / 2 + (float)x1, pictureBox1.Height / 2 + (float)y1);
+
+        }
+    }
+}
